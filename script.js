0419e035ecf5035e65e901e7b511725f60b8c005
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-02-06 00:40:03 +0100

    updt

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 function CloseMenu() {
-    document.getElementById("left-nav-mob").style.display = "none";
-    document.getElementById("open-menu").style.display = "block";
-    document.getElementById("close-menu").style.display = "none";
-  }
-  function OpenMenu() {
-    document.getElementById("left-nav-mob").style.display = "initial";
-    document.getElementById("close-menu").style.display = "block";
-    document.getElementById("open-menu").style.display = "none";
-  }
+  document.getElementById("left-nav-mob").style.display = "none";
+  document.getElementById("open-menu").style.display = "block";
+  document.getElementById("close-menu").style.display = "none";
+}
+function OpenMenu() {
+  document.getElementById("left-nav-mob").style.display = "initial";
+  document.getElementById("close-menu").style.display = "block";
+  document.getElementById("open-menu").style.display = "none";
+}
 
-  function LogOut() {
-    document.getElementById("logout").style.display = "initial";
-    document.getElementById("showlog").style.display = "none";
-    document.getElementById("hidelog").style.display = "initial";
-  }
+function LogOut() {
+  document.getElementById("logout").style.display = "initial";
+  document.getElementById("showlog").style.display = "none";
+  document.getElementById("hidelog").style.display = "initial";
+}
 
-  function HideLog() {
-    document.getElementById("logout").style.display = "none";
-    document.getElementById("showlog").style.display = "initial";
-    document.getElementById("hidelog").style.display = "none";
-  }
+function HideLog() {
+  document.getElementById("logout").style.display = "none";
+  document.getElementById("showlog").style.display = "initial";
+  document.getElementById("hidelog").style.display = "none";
+}
 
-  function Delete() {
-    var txt;
+function SearchPost() {
+  document.getElementById("search-result").style.display = "block";
+}
+function DeletePost() {
+  document.getElementById("search-result").style.display = "none";
+  var txt;
   if (confirm("Are you sure you want to delete?")) {
     txt = "You confirmed ";
   } else {
     txt = "You Cancel!";
   }
-  }
+}
 
-  function ConfirmLogOut() {
-    var txt;
+function ConfirmLogOut() {
+  var txt;
   if (confirm("Are you sure you want to logout?")) {
     txt = "You confirmed ";
   } else {
     txt = "You Cancel!";
   }
-  }
+}
+
+// height: 100%;
